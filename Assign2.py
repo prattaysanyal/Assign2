b1871c70b96684f0bfe516b37aa3e797e4358d1e
prattaysanyal <40462781+prattaysanyal@users.noreply.github.com>
2018-06-23 18:49:59 +0530

    Add files via upload

---
+++ Assign2.py
@@ -0,0 +1,44 @@
+// Q1. Print anything you want on screen. //
+print("Prattay can crack Google Interview")
+
+// Q2. Join two strings using '+'. //
+print("Enter 'x' for exit.");
+string1 = input("Enter first string to join: ");
+if string1 == 'x':
+    exit();
+else:
+    string2 = input("Enter second string to join: ");
+    string3 = string1 + string2;
+    print("\nString after join =",string3);
+    print("String 1 =",string1);
+    print("String 2 =",string2);
+    print("String 3 =",string3);
+
+// Q3. Take the input of 3 variables x,y,z. Print their values on screen.
+x = int(input("Enter the value of x:"))
+y = int(input("Enter the value of y:"))
+z = int(input("Enter the value of z:"))
+print(x)
+print(y)
+print(z)
+
+// Q4. Print "Let's get started" on screen. //
+print("Let's get started")
+
+// Q5. Print the following values using placeholders. //
+s="Acadview"
+course="Python"
+fees=5000
+print("s= %s")%(Acadview))       
+print("course= %s")%(course))
+print("fees= %d")%(fees))
+
+// Q6. Let's do some interesting exercise:(Fill in the blanks). //
+name="Tony Stark"
+salary=1000000
+print('%s"%d')%(name,salary)
+
+
+
+
+    
